0086e398f3f4af5fe6a4267b6d7d83c38aca0198
Carl Schwan <carl@carlschwan.eu>
2023-07-29 10:24:37 +0200

    Export more symbols to make this library usable in Kube

--- src/core/mailtemplates.h
+++ src/core/mailtemplates.h
@@ -9,6 +9,7 @@
 #include <functional>
 
 #include "mailcrypto.h"
+#include "mimetreeparser_core_export.h"
 
 struct Attachment {
     QString name;
@@ -26,25 +27,24 @@ struct Recipients {
 
 namespace MailTemplates
 {
-void reply(const KMime::Message::Ptr &origMsg,
-           const std::function<void(const KMime::Message::Ptr &result)> &callback,
-           const KMime::Types::AddrSpecList &me = {});
-void forward(const KMime::Message::Ptr &origMsg, const std::function<void(const KMime::Message::Ptr &result)> &callback);
-QString plaintextContent(const KMime::Message::Ptr &origMsg);
-QString body(const KMime::Message::Ptr &msg, bool &isHtml);
-KMime::Message::Ptr createMessage(KMime::Message::Ptr existingMessage,
-                                  const QStringList &to,
-                                  const QStringList &cc,
-                                  const QStringList &bcc,
-                                  const KMime::Types::Mailbox &from,
-                                  const QString &subject,
-                                  const QString &body,
-                                  bool htmlBody,
-                                  const QList<Attachment> &attachments,
-                                  const std::vector<Crypto::Key> &signingKeys = {},
-                                  const std::vector<Crypto::Key> &encryptionKeys = {},
-                                  const Crypto::Key &attachedKey = {});
+MIMETREEPARSER_CORE_EXPORT void
+reply(const KMime::Message::Ptr &origMsg, const std::function<void(const KMime::Message::Ptr &result)> &callback, const KMime::Types::AddrSpecList &me = {});
+MIMETREEPARSER_CORE_EXPORT void forward(const KMime::Message::Ptr &origMsg, const std::function<void(const KMime::Message::Ptr &result)> &callback);
+MIMETREEPARSER_CORE_EXPORT QString plaintextContent(const KMime::Message::Ptr &origMsg);
+MIMETREEPARSER_CORE_EXPORT QString body(const KMime::Message::Ptr &msg, bool &isHtml);
+MIMETREEPARSER_CORE_EXPORT KMime::Message::Ptr createMessage(KMime::Message::Ptr existingMessage,
+                                                             const QStringList &to,
+                                                             const QStringList &cc,
+                                                             const QStringList &bcc,
+                                                             const KMime::Types::Mailbox &from,
+                                                             const QString &subject,
+                                                             const QString &body,
+                                                             bool htmlBody,
+                                                             const QList<Attachment> &attachments,
+                                                             const std::vector<Crypto::Key> &signingKeys = {},
+                                                             const std::vector<Crypto::Key> &encryptionKeys = {},
+                                                             const Crypto::Key &attachedKey = {});
 
-KMime::Message::Ptr
+MIMETREEPARSER_CORE_EXPORT KMime::Message::Ptr
 createIMipMessage(const QString &from, const Recipients &recipients, const QString &subject, const QString &body, const QString &attachment);
 };
