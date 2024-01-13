432bbe422b28115ac5564ad2a7745b4da88255f1
Yuri Chornoivan <yurchor@ukr.net>
2024-01-13 09:50:56 +0200

    Fix minor typo

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -43,7 +43,7 @@ QString MimeTreeParser::decryptRecipientsToHtml(const std::vector<std::pair<GpgM
             const auto link = QStringLiteral("messageviewer:showCertificate#%1 ### %2 ### %3")
                                   .arg(cryptoProto->displayName(), cryptoProto->name(), QString::fromLatin1(recipient.keyID()));
             text +=
-                QStringLiteral("<li>%1 (<a href=\"%2\">%3</a>)</li>").arg(i18nc("@info", "Unknow Key"), link, Kleo::Formatting::prettyID(recipient.keyID()));
+                QStringLiteral("<li>%1 (<a href=\"%2\">%3</a>)</li>").arg(i18nc("@info", "Unknown Key"), link, Kleo::Formatting::prettyID(recipient.keyID()));
         }
     }
     text += QStringLiteral("</ul>");
