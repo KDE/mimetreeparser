64df9f92a61a3985954e0ef45eb7568a6d36bd13
Carl Schwan <carl@carlschwan.eu>
2024-06-27 23:01:48 +0200

    Port to declarative type registration

---
+++ src/quick/types.h
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2024 Carl Schwan <carl@carlschwan.eu>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#include <MimeTreeParserCore/AttachmentModel>
+#include <MimeTreeParserCore/MessageParser>
+#include <MimeTreeParserCore/PartModel>
+
+#include <QtQml/qqmlregistration.h>
+
+class MessageParserForeign : public QObject
+{
+    Q_OBJECT
+    QML_NAMED_ELEMENT(MessageParser)
+    QML_FOREIGN(MessageParser)
+};
+
+class PartModelForeign : public QObject
+{
+    Q_OBJECT
+    QML_NAMED_ELEMENT(PartModel)
+    QML_UNCREATABLE("Enum only")
+    QML_FOREIGN(PartModel)
+};
+
+class AttachmentModelForeign : public QObject
+{
+    Q_OBJECT
+    QML_NAMED_ELEMENT(AttachmentModel)
+    QML_UNCREATABLE("Enum only")
+    QML_FOREIGN(AttachmentModel)
+};
