c7cd3c50eb15d8b3b3f6af1c93ed9e2f546768bd
Carl Schwan <carl@carlschwan.eu>
2024-07-01 09:20:26 +0200

    Fix licensing of src/quick/types.h

--- src/quick/types.h
+++ src/quick/types.h
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2024 Carl Schwan <carl@carlschwan.eu>
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include <MimeTreeParserCore/AttachmentModel>
 #include <MimeTreeParserCore/MessageParser>
