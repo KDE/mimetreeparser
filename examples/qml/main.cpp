91575c9ba93c0291db3b47f5a5437118e4697cf4
Laurent Montel <montel@kde.org>
2024-10-14 07:12:34 +0200

    Use KLocalizedQmlContext

--- examples/qml/main.cpp
+++ examples/qml/main.cpp
@@ -15,6 +15,9 @@
 #include <KLocalizedContext>
 #include <MimeTreeParserCore/FileOpener>
 #include <MimeTreeParserCore/MessageParser>
+#if KI18N_VERSION >= QT_VERSION_CHECK(6, 8, 0)
+#include <KLocalizedQmlContext>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -25,7 +28,11 @@ int main(int argc, char *argv[])
     parser.addPositionalArgument(QStringLiteral("file"), i18n("mbox file"));
 
     QQmlApplicationEngine engine;
+#if KI18N_VERSION < QT_VERSION_CHECK(6, 8, 0)
     engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
+#else
+    engine.rootContext()->setContextObject(new KLocalizedQmlContext(&engine));
+#endif
     QObject::connect(&engine, &QQmlApplicationEngine::quit, &app, &QCoreApplication::quit);
 
     constexpr auto uri = "org.kde.mimetreeparser";
