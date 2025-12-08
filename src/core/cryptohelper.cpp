d48e35926f447723125cdb468cfd383f2cfe09a4
Volker Krause <vkrause@kde.org>
2025-12-08 17:45:40 +0100

    Adapt to KMime header factory API changes

--- src/core/cryptohelper.cpp
+++ src/core/cryptohelper.cpp
@@ -176,10 +176,10 @@ void copyHeader(const KMime::Headers::Base *header, KMime::Message::Ptr msg)
 {
     auto newHdr = KMime::Headers::createHeader(header->type());
     if (!newHdr) {
-        newHdr = new KMime::Headers::Generic(header->type());
+        newHdr = std::make_unique<KMime::Headers::Generic>(header->type());
     }
-    newHdr->from7BitString(header->as7BitString(false));
-    msg->appendHeader(newHdr);
+    newHdr->from7BitString(header->as7BitString());
+    msg->appendHeader(std::move(newHdr));
 }
 
 [[nodiscard]] bool isContentHeader(const KMime::Headers::Base *header)
