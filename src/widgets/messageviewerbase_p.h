75a83b05c91a471825c3d13a68f2ac66c7396c68
Ingo Klöcker <dev@ingo-kloecker.de>
2026-03-26 15:11:11 +0100

    Unify update of UI when messages were changed
    Updating the UI (when the messages were changed or first set) is moved
    from MessageViewerWindow to the (private) base class. This allows us to
    remove the update of the UI from MessageViewerDialog's initGUI().

--- src/widgets/messageviewerbase_p.h
+++ src/widgets/messageviewerbase_p.h
@@ -29,31 +29,47 @@ class MessageViewer;
 
 class MessageViewerBasePrivate
 {
-public:
-    explicit MessageViewerBasePrivate(QWidget *qq);
+    static constexpr int CURRENT_INDEX_NOT_INITIALIZED = -2;
+    static constexpr int CURRENT_INDEX_NO_MESSAGES = -1;
 
     QWidget *const q;
-    int currentIndex = 0;
-    QList<std::shared_ptr<KMime::Message>> messages;
+
+protected:
+    explicit MessageViewerBasePrivate(QWidget *qq);
+
+public:
+    inline QList<std::shared_ptr<KMime::Message>> getMessages() const
+    {
+        return messages;
+    }
+
+    // return true if the messages were actually changed
+    bool setMessages(const QList<std::shared_ptr<KMime::Message>> &messages);
+
+public:
     QString fileName;
+    // the central widget is created by the constructor
     QStackedWidget *centralWidget = nullptr;
+    // the message viewer is created by the constructor
     MimeTreeParser::Widgets::MessageViewer *messageViewer = nullptr;
-    KMessageWidget *errorMessage = nullptr;
+    // all actions are created by the constructor; they are disabled by default
     QAction *saveAction = nullptr;
     QAction *saveDecryptedAction = nullptr;
     QAction *printPreviewAction = nullptr;
     QAction *printAction = nullptr;
     QAction *nextAction = nullptr;
     QAction *previousAction = nullptr;
+    // the tool bar is created by the constructor
     QToolBar *toolBar = nullptr;
+    // the status bar is created by the constructor only if needed, i.e. it can be nullptr
     QStatusBar *statusBar = nullptr;
 
-    void setCurrentIndex(int currentIndex);
-
-    void createToolBar(QWidget *parent);
-
 private:
+    void setCurrentIndex(int currentIndex);
+    void updateUI();
+
     void createActions(QWidget *parent);
+    void createToolBar(QWidget *parent);
     void createStatusBar(QWidget *parent);
 
     void save(QWidget *parent);
@@ -61,4 +77,9 @@ private:
     void print(QWidget *parent);
     void printPreview(QWidget *parent);
     void printInternal(QPrinter *printer);
+
+private:
+    int currentIndex = CURRENT_INDEX_NOT_INITIALIZED;
+    QList<std::shared_ptr<KMime::Message>> messages;
+    KMessageWidget *errorMessage = nullptr;
 };
