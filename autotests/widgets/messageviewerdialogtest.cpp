bc2c5e010fd9048ebb97dfab76b85222e1c56f5b
Ingo Klöcker <dev@ingo-kloecker.de>
2026-04-07 14:04:56 +0200

    Fix and extend tests for MessageViewerDialog

--- autotests/widgets/messageviewerdialogtest.cpp
+++ autotests/widgets/messageviewerdialogtest.cpp
@@ -3,8 +3,13 @@
 // SPDX-License-Identifier: LGPL-2.0-or-later
 
 #include <MimeTreeParserWidgets/MessageViewerDialog>
+
+#include <QDialogButtonBox>
 #include <QLayout>
+#include <QMenu>
+#include <QStackedWidget>
 #include <QTest>
+#include <QToolBar>
 
 using namespace MimeTreeParser::Widgets;
 
@@ -18,12 +23,31 @@ private Q_SLOTS:
         MessageViewerDialog dialog(QLatin1StringView(MAIL_DATA_DIR) + QLatin1Char('/') + QLatin1StringView("combined.mbox"));
         QCOMPARE(dialog.messages().count(), 3);
 
-        QCOMPARE(dialog.layout()->count(), 2);
-        QVERIFY(dialog.layout()->itemAt(1)->layout());
-        QCOMPARE(dialog.layout()->itemAt(1)->layout()->count(), 2);
+        QCOMPARE(dialog.layout()->count(), 3); // toolbar, central widget, button box
 
-        const auto actions = dialog.layout()->menuBar()->actions();
-        QCOMPARE(actions.count(), 2);
+        const auto toolBar = qobject_cast<QToolBar *>(dialog.layout()->itemAt(0)->widget());
+        QVERIFY(toolBar);
+        QVERIFY(!toolBar->isHidden());
+
+        const auto centralWidget = qobject_cast<QStackedWidget *>(dialog.layout()->itemAt(1)->widget());
+        QVERIFY(centralWidget);
+        QCOMPARE(centralWidget->currentIndex(), 0);
+
+        const auto buttonBox = qobject_cast<QDialogButtonBox *>(dialog.layout()->itemAt(2)->widget());
+        QVERIFY(buttonBox);
+
+        const auto menuActions = dialog.layout()->menuBar()->actions();
+        QCOMPARE(menuActions.count(), 3); // File, View, Navigation
+        const auto fileActions = menuActions[0]->menu()->actions();
+        QCOMPARE(fileActions.size(), 4);
+        QVERIFY(std::ranges::all_of(fileActions, std::mem_fn(&QAction::isEnabled)));
+        const auto viewActions = menuActions[1]->menu()->actions();
+        QCOMPARE(viewActions.size(), 2);
+        QVERIFY(std::ranges::all_of(viewActions, std::mem_fn(&QAction::isEnabled)));
+        const auto navigationActions = menuActions[2]->menu()->actions();
+        QCOMPARE(navigationActions.size(), 2);
+        QVERIFY(!navigationActions[0]->isEnabled());
+        QVERIFY(navigationActions[1]->isEnabled());
     }
 
     void messageViewerDialogCreationTest()
@@ -31,12 +55,55 @@ private Q_SLOTS:
         MessageViewerDialog dialog(QLatin1StringView(MAIL_DATA_DIR) + QLatin1Char('/') + QLatin1StringView("plaintext.mbox"));
         QCOMPARE(dialog.messages().count(), 1);
 
-        QCOMPARE(dialog.layout()->count(), 2);
+        QCOMPARE(dialog.layout()->count(), 3); // toolbar, central widget, button box
 
-        QVERIFY(!dialog.layout()->itemAt(0)->widget()->isVisible());
+        const auto toolBar = qobject_cast<QToolBar *>(dialog.layout()->itemAt(0)->widget());
+        QVERIFY(toolBar);
+        QVERIFY(toolBar->isHidden());
 
-        QVERIFY(dialog.layout()->itemAt(1)->layout());
-        QCOMPARE(dialog.layout()->itemAt(1)->layout()->count(), 2);
+        const auto centralWidget = qobject_cast<QStackedWidget *>(dialog.layout()->itemAt(1)->widget());
+        QVERIFY(centralWidget);
+        QCOMPARE(centralWidget->currentIndex(), 0);
+
+        const auto buttonBox = qobject_cast<QDialogButtonBox *>(dialog.layout()->itemAt(2)->widget());
+        QVERIFY(buttonBox);
+
+        const auto menuActions = dialog.layout()->menuBar()->actions();
+        QCOMPARE(menuActions.count(), 3); // File, View, Navigation
+        const auto fileActions = menuActions[0]->menu()->actions();
+        QCOMPARE(fileActions.size(), 4);
+        QVERIFY(std::ranges::all_of(fileActions, std::mem_fn(&QAction::isEnabled)));
+        const auto viewActions = menuActions[1]->menu()->actions();
+        QCOMPARE(viewActions.size(), 2);
+        QVERIFY(std::ranges::all_of(viewActions, std::mem_fn(&QAction::isEnabled)));
+    }
+
+    void messageViewerDialogCreationNoMessagesTest()
+    {
+        MessageViewerDialog dialog(QLatin1StringView(MAIL_DATA_DIR) + QLatin1Char('/') + QLatin1StringView("empty.mbox"));
+        QCOMPARE(dialog.messages().count(), 0);
+
+        QCOMPARE(dialog.layout()->count(), 3); // toolbar, central widget, button box
+
+        const auto toolBar = qobject_cast<QToolBar *>(dialog.layout()->itemAt(0)->widget());
+        QVERIFY(toolBar);
+        QVERIFY(toolBar->isHidden());
+
+        const auto centralWidget = qobject_cast<QStackedWidget *>(dialog.layout()->itemAt(1)->widget());
+        QVERIFY(centralWidget);
+        QCOMPARE(centralWidget->currentIndex(), 1);
+
+        const auto buttonBox = qobject_cast<QDialogButtonBox *>(dialog.layout()->itemAt(2)->widget());
+        QVERIFY(buttonBox);
+
+        const auto menuActions = dialog.layout()->menuBar()->actions();
+        QCOMPARE(menuActions.count(), 3); // File, View, Navigation
+        const auto fileActions = menuActions[0]->menu()->actions();
+        QCOMPARE(fileActions.size(), 4);
+        QVERIFY(std::ranges::none_of(fileActions, std::mem_fn(&QAction::isEnabled)));
+        const auto viewActions = menuActions[1]->menu()->actions();
+        QCOMPARE(viewActions.size(), 2);
+        QVERIFY(std::ranges::none_of(viewActions, std::mem_fn(&QAction::isEnabled)));
     }
 };
 
